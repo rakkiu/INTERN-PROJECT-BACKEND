7814d444b398ddf53b9a298956c6e16feb6303aa
rakkiu <votuankhoi02102004@gmail.com>
2026-02-04 10:13:30 +0700

    first commit

---
+++ Infrastructure/Identity/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Identity
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+  
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        public DbSet<JwtToken> JwtTokens { get; set; } 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure relationships if needed, e.g. composite keys
+            // modelBuilder.Entity<ProductCategory>().HasKey(pc => new { pc.ProductId, pc.CategoryId });
+        }
+    }
+}
